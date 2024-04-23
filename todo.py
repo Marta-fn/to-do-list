c7acd1e84232a277456641ad11daa8c491d86d19
Marta <160023329+Marta-fn@users.noreply.github.com>
2024-04-23 12:49:44 +0100

    Updated todo.py
    • Added a function to mark task as done
    • Updated GUI window size and widgets font and colors

--- todo.py
+++ todo.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import filedialog
 from ttkbootstrap import Style
 
@@ -6,43 +7,69 @@ class ToDoListApp(tk.Tk):
   def __init__(self):
     super().__init__()
 
-    self.title("My To-do List App")
-    self.geometry("400x400")
-    style = Style(theme="minty")
-    style.configure("Custon.TEntry", foreground="white")
+    self.title("My To-do List")
+    self.iconbitmap("task.ico")
+    self.geometry("400x515")
+    self.resizable(False,False)
+    style = Style()
+    style.theme_use("minty")
+    button_font = ttk.Style()
+    button_font.configure(".", font=("Roboto", 16))
 
     self.create_widgets()
   
   def create_widgets(self):
-    self.task_input = tk.Entry(self, width=38)
+
+    self.task_input = ttk.Entry(self, width=30, font=("Roboto", 16))
     self.task_input.pack(pady=10)
 
-    self.add_task_buttom = tk.Button(self, text="Add Task", command=self.add_task)
+    self.task_input.insert(0, "Write your tasks here...")
+    self.task_input.bind("<FocusIn>", self.clear_placeholder)
+    self.task_input.bind("<FocusOut>", self.restore_placeholder)
+
+    self.add_task_buttom = ttk.Button(self, text="Add Task", command=self.add_task, style="success.TButton")
     self.add_task_buttom.pack(pady=5)
 
-    self.task_listbox = tk.Listbox(self, selectmode=tk.SINGLE)
+    self.task_listbox = tk.Listbox(self, selectmode=tk.SINGLE, width=30, font=("Roboto", 16))
     self.task_listbox.pack(pady=5)
+
+    self.done_task = ttk.Button(self, text="Done", style="success.TButton", command=self.mark_done)
+    self.done_task.pack(pady=5)
 
     self.button_frame = tk.Frame(self)
     self.button_frame.pack(pady=5)
 
-    self.edit_task_button = tk.Button(self.button_frame, text="Edit Text", command=self.edit_task)
+    self.edit_task_button = ttk.Button(self.button_frame, text="Edit Task", command=self.edit_task, style="info.TButton")
     self.edit_task_button.grid(row=0, column=0, padx=5)
 
-    self.delete_task_button = tk.Button(self.button_frame, text="Delete task", command=self.delete_task)
+    self.delete_task_button = ttk.Button(self.button_frame, text="Delete task", command=self.delete_task, style="danger.TButton")
     self.delete_task_button.grid(row=0, column=1, padx=5)
 
-    self.save_button = tk.Button(self, text="Save", command=self.save_tasks)
-    self.save_button.pack(pady=5)
+    self.save_button = ttk.Button(self.button_frame, text="Save", command=self.save_tasks)
+    self.save_button.grid(row=1, column=0, padx=5, pady=5)
 
-    self.load_button = tk.Button(self, text="Load", command=self.load_tasks)
-    self.load_button.pack(pady=5)
+    self.load_button = ttk.Button(self.button_frame, text="Load", command=self.load_tasks)
+    self.load_button.grid(row=1, column=1, padx=5, pady=5)
+
+  def clear_placeholder(self, event):
+    if self.task_input.get() == "Write your tasks here...":
+      self.task_input.delete(0, tk.END)
+
+  def restore_placeholder(self, event):
+    if not self.task_input.get():
+      self.task_input.insert(0, "Write your tasks here...")  
 
   def add_task(self):
     task = self.task_input.get()
     if task:
       self.task_listbox.insert(tk.END, task)
       self.task_input.delete(0, tk.END)
+      self.restore_placeholder(None)
+
+  def mark_done(self):
+    task_index = self.task_listbox.curselection()
+    if task_index:
+      self.task_listbox.itemconfigure(task_index, fg="#78c2ad")
 
   def edit_task(self):
     task_index = self.task_listbox.curselection()
@@ -52,7 +79,8 @@ class ToDoListApp(tk.Tk):
         self.task_listbox.delete(task_index)
         self.task_listbox.insert(task_index, new_task)
         self.task_input.delete(0, tk.END)
-  
+        self.restore_placeholder(None)
+
   def delete_task(self):
     task_index = self.task_listbox.curselection()
     if task_index:
